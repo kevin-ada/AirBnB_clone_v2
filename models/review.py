79f87afda2c8bfb75351304d1f0d6fbdc26c76fd
kim-thuita <kevinkoech265@gmail.com>
2023-08-24 15:44:21 +0300

    " DBStorage States and Cities"

--- models/review.py
+++ models/review.py
@@ -3,13 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
+from models import type_storage
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    if storage_type == 'db':
+    if type_storage == 'db':
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
+from models import type_storage
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,7 +10,7 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class or table model """
     __tablename__ = 'states'
-    if storage_type == 'db':
+    if type_storage == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
