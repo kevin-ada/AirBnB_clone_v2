f742c27eca6684fe74b1c8b7faffb0239b55dd86
kim-thuita <kevinkoech265@gmail.com>
2023-08-26 23:22:40 +0300

    " Fixing circular import issues"

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" Test link Many-To-Many Place <> Amenity
 """
-from models.base_model import BaseModel
+Test link Many-To-Many Place <> Amenity
+"""
 from models import storage
-from models.user import User
+from models.amenity import Amenity
 from models.place import Place
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+from models.user import User
+
+
 
 # creation of a State
 state = State(name="California")
