ecd5bdba1c6239e8463a88618c4da65933a3b672
benkivuva <benkivuvak5@gmail.com>
2023-09-07 08:10:15 -0700

    cr

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-""" Cleans up outdated web_static archives and directories """
+""" Cleans deployment"""
 
 from fabric.api import *
+import os
+from datetime import datetime
+import tarfile
 
 env.hosts = ["100.25.119.2", "100.25.136.71"]
 env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """
-    Cleans up outdated web_static archives and directories.
-
-    Args:
-        number (int): The number of archives to keep, including the most recent.
-
-    Notes:
-        If number is 0 or 1, only the most recent version of the archive is kept.
-        If number is 2, the most recent and second most recent versions are kept, and so on.
-    """
+    """ Removes all but given number of archives"""
     number = int(number)
-    if number < 1:
+    if number < 2:
         number = 1
-
+    number += 1
+    number = str(number)
     with lcd("versions"):
-        local("ls -t | tail -n +{} | xargs -I {{}} rm {{}}".format(number))
-
+        local("ls -1t | grep web_static_.*\.tgz | tail -n +" +
+              number + " | xargs -I {} rm -- {}")
     with cd("/data/web_static/releases"):
-        run("ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}".format(number))
+        run("ls -1t | grep web_static_ | tail -n +" +
+            number + " | xargs -I {} rm -rf -- {}")
