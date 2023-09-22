ed6a6b0661026729cc995f6346a2b537ca50d1f6
benkivuva <benkivuvak5@gmail.com>
2023-09-22 08:12:33 -0700

    pep 8 fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,6 +9,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -16,12 +17,14 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Render the 'HBNB' page.
     """
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
